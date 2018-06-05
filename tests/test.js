3b198358c271fcb8cff1f3b3a62463085ff6d72a
Alexander Ivanov <aivanov@informatica.ru>
2018-06-05 15:43:39 +0300

    fix test

--- tests/test.js
+++ tests/test.js
@@ -1,7 +1,7 @@
 var assert = require('assert');
 
 describe('ready', () => {
-    it('return true after 3sec', () => {
+    it('return true after 3sec', function () {
       // should set the timeout of this test to 1000 ms; instead will fail
       this.timeout(3000);
       assert.ok(true);
