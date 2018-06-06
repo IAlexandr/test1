bba4b8b12b5edce780d4a0f6fd995b2cdff78e8c
Alexander Ivanov <aivanov@informatica.ru>
2018-06-06 09:19:01 +0300

    express server, docker-compose

--- dockerfile
+++ dockerfile
@@ -3,6 +3,7 @@ FROM node:10.1.0-alpine
 ARG DIR=/home/test1
 ADD package.json $DIR/
 WORKDIR $DIR
+RUN npm install
 
 COPY . $DIR
 
--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log('1');
+const express = require('express');
+const http = require('http');
+
+const app = express();
+app.get('/test', (req, res) => res.json({test: 'ok'}));
+
+const PORT = process.env['PORT'] || 5000;
+const httpServer = http.Server(app);
+httpServer.listen(PORT);
+console.log(`listening ${PORT}`);
